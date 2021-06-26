339145279d93076aa5c6f57991a8374a07e569cf
Viktoria <doduchenko29@gmail.com>
2021-06-26 14:34:41 +0300

    add

---
+++ index.js
@@ -0,0 +1,39 @@
+const baseUrl = 'https://60d5f912943aa60017768d3c.mockapi.io/api/v1/users';
+
+export function getUsersList() {
+  return fetch(baseUrl).then((responce) => responce.json())
+   
+}
+
+export function getUserById(userId) {
+  return fetch(
+    `https://60d5f912943aa60017768d3c.mockapi.io/api/v1/users/${userId}`
+  ).then((responce) => responce.json());
+}
+
+export function createUser(userData) {
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(userData)
+  });
+}
+
+export function deleteUser(userId) {
+  fetch(`${baseUrl}/${userId}`, {
+    method: 'DELETE',
+  })
+}
+
+export function updateUser(userId, userData) {
+  fetch(`${baseUrl}/${userId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(userData)
+  });
+}
+
